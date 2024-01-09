7a2f7342ae40a618d59084ad15975590e3787801
michalforman <miska.forman@gmail.com>
2024-01-09 17:03:57 +0100

    Initial Commit

---
+++ Smart Weather Iphone/Smart_Weather_IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Weather_IphoneApp.swift
+//  Smart Weather Iphone
+//
+//  Created by Michal Forman on 09.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Weather_IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
